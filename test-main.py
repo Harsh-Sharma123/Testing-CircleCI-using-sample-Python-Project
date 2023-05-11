9348ddd638dd853e9ed3697bca7731f038b41375
Harsh Sharma <hs804506@gmail.com>
2023-05-11 15:02:53 +0530

    Added test to make sure 5+5 is 10

--- test-main.py
+++ test-main.py
@@ -2,8 +2,9 @@ from main import add
 
 def testAdd():
     assert add(2, 3) == 5
-    assert add(5, 5) == 10
     print("Add function works fine!")
+    assert add(5, 5) == 10
+    
 
 if __name__ == '__main__':
     testAdd()
